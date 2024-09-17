583bb465eb5fa7d1958669837c42ce586e509e95
Soni kumari <Sonikumari78707@gmail.com>
2024-09-17 22:44:30 +0530

    start code

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const score = document.querySelector('#score')
+const num1 = document.querySelector('#num1')
+const num2 = document.querySelector('#num2')
+const inp = document.querySelector('#input')
+const sbt = document.querySelector('#submit')
+
